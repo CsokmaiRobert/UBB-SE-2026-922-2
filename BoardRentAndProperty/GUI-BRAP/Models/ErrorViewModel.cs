e8524143ba56bac45f3ed4c667d84c22c813b36f
Cucuteanu Tudor 912 <71983139+Cucuteanu-Tudor-912@users.noreply.github.com>
2026-05-07 14:21:17 +0300

    start

---
+++ BoardRentAndProperty/GUI-BRAP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GUI_BRAP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
