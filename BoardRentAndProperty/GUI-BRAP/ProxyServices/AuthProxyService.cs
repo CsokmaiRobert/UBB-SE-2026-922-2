93fc596c7550fb700c8b8e508dd732e2b61e3e4a
CosovanuGabi912 <marius.cosovanu@stud.ubbcluj.ro>
2026-05-13 15:23:09 +0300

    Initial commit with diagram and code

--- BoardRentAndProperty/GUI-BRAP/ProxyServices/AuthProxyService.cs
+++ BoardRentAndProperty/GUI-BRAP/ProxyServices/AuthProxyService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading;
@@ -20,8 +21,78 @@ namespace GUI_BRAP.ProxyServices
         public async Task<AccountProfileDataTransferObject> LoginAsync(LoginDataTransferObject body, CancellationToken cancellationToken = default)
         {
             HttpClient client = this.httpClientFactory.CreateClient(ApiClientNames.BoardRentApi);
-            using HttpResponseMessage response = await client.PostAsJsonAsync("api/auth/login", body, cancellationToken);
-            return await HttpResponseEnsurer.ReadJsonAsync<AccountProfileDataTransferObject>(response, cancellationToken);
+            HttpResponseMessage response;
+            try
+            {
+                response = await client.PostAsJsonAsync("api/auth/login", body, cancellationToken);
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new ProxyServiceException("Cannot connect to the API.", HttpStatusCode.ServiceUnavailable, apiErrorCode: null, ex);
+            }
+
+            using (response)
+            {
+                return await HttpResponseEnsurer.ReadJsonAsync<AccountProfileDataTransferObject>(response, cancellationToken);
+            }
+        }
+
+        public async Task RegisterAsync(RegisterDataTransferObject body, CancellationToken cancellationToken = default)
+        {
+            HttpClient client = this.httpClientFactory.CreateClient(ApiClientNames.BoardRentApi);
+            HttpResponseMessage response;
+            try
+            {
+                response = await client.PostAsJsonAsync("api/auth/register", body, cancellationToken);
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new ProxyServiceException("Cannot connect to the API.", HttpStatusCode.ServiceUnavailable, apiErrorCode: null, ex);
+            }
+
+            using (response)
+            {
+                await HttpResponseEnsurer.EnsureSuccessAsync(response, cancellationToken);
+            }
+        }
+
+        public async Task LogoutAsync(CancellationToken cancellationToken = default)
+        {
+            HttpClient client = this.httpClientFactory.CreateClient(ApiClientNames.BoardRentApi);
+            HttpResponseMessage response;
+            try
+            {
+                response = await client.PostAsync("api/auth/logout", content: null, cancellationToken);
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new ProxyServiceException("Cannot connect to the API.", HttpStatusCode.ServiceUnavailable, apiErrorCode: null, ex);
+            }
+
+            using (response)
+            {
+                await HttpResponseEnsurer.EnsureSuccessAsync(response, cancellationToken);
+            }
+        }
+
+        public async Task<string> ForgotPasswordAsync(CancellationToken cancellationToken = default)
+        {
+            HttpClient client = this.httpClientFactory.CreateClient(ApiClientNames.BoardRentApi);
+            HttpResponseMessage response;
+            try
+            {
+                response = await client.GetAsync("api/auth/forgot-password", cancellationToken);
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new ProxyServiceException("Cannot connect to the API.", HttpStatusCode.ServiceUnavailable, apiErrorCode: null, ex);
+            }
+
+            using (response)
+            {
+                await HttpResponseEnsurer.EnsureSuccessAsync(response, cancellationToken);
+                return await response.Content.ReadAsStringAsync(cancellationToken);
+            }
         }
     }
 }
